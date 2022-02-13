cd8e12f63e040825974ee77c37381f024e5f13e2
1stmuse <olysegs@gmail.com>
2022-02-13 10:43:30 +0100

    edited READ.MD file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ClNewApp
+# MOBILE SOFTWARE DEVELOPER TEST SOLUTION
+
+# This solution has only been tested on the Android platform
+
+# To get the App start do the following;
+
+    "*" clone the repo or copy this link https://github.com/1stmuse/ClNewApp.git
+
+    "*" from your device terminal run "git clone https://github.com/1stmuse/ClNewApp.git"
+
+    "*" cd into the folder and run "yarn" to install all dependencies
+
+    "*" from the root folder run "yarn android" to build the app
+    "*" run "yarn start" to start the app.
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text, FlatList } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import Feather from "react-native-vector-icons/Feather";
 import Header from "../components/Header";
@@ -7,11 +13,20 @@ import { HEADER_HEIGHT, DEVICE_HEIGHT } from "../utils/constants";
 import NewsCard from "../components/NewsCard";
 import Pagination from "../components/Pagination";
 import colors from "../utils/colors";
+import AppButton from "../components/AppButton";
 
 const Home = ({ navigation }) => {
   const dispatch = useDispatch();
   const news = useSelector((state) => state.news);
+  const loadingState = useSelector(
+    (state) => state.loading.effects.news.getAllNewsAsync
+  );
   const [pageNumber, setPageNumber] = useState(1);
+  console.log(loadingState);
+
+  const tryAgain = async () => {
+    await dispatch.news.getAllNewsAsync(pageNumber);
+  };
 
   useEffect(() => {
     dispatch.news.getAllNewsAsync(pageNumber);
@@ -38,14 +53,26 @@ const Home = ({ navigation }) => {
           />
         )}
       />
-      <View>
-        <FlatList
-          contentContainerStyle={styles.flatlist}
-          data={news}
-          renderItem={({ item }) => <NewsCard data={item} />}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
+      {loadingState ? (
+        <View style={styles.center}>
+          <ActivityIndicator animating={true} color={colors.red} />
+        </View>
+      ) : null}
+      {!loadingState && !news.length ? (
+        <View style={styles.center}>
+          <Text style={styles.errorMsg}>There seems to be an Error</Text>
+          <AppButton title="Try Again" onPress={tryAgain} />
+        </View>
+      ) : (
+        <View>
+          <FlatList
+            contentContainerStyle={styles.flatlist}
+            data={news}
+            renderItem={({ item }) => <NewsCard data={item} />}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+      )}
       <View style={styles.pagination}>
         <Pagination page={pageNumber} setPage={setPageNumber} />
       </View>
@@ -66,6 +93,15 @@ const styles = StyleSheet.create({
     bottom: DEVICE_HEIGHT * 0,
     left: 0,
     right: 0,
+  },
+  center: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  errorMsg: {
+    fontSize: 17,
+    marginBottom: 10,
   },
 });
 export default Home;
